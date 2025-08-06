36bcea5cf55a35829a93155eb7aaeeb3c8c67f85
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-08-06 18:03:28 +0300

    ...

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,3 @@
-// Elements
 const modal1 = document.getElementById('myModal');
 const modal2 = document.getElementById('myModal2');
 const closeBtn1 = document.getElementById('closeModalBtn');
@@ -7,26 +6,22 @@ const saveBtn = document.querySelector('.save__btn');
 const input = document.getElementById('username');
 const userNameDisplay = document.querySelector('.header__text__user-name');
 
-// Show modal 1 on page load
+
 modal1.style.display = 'block';
 
-// Close modal 1 (X)
 closeBtn1.addEventListener('click', () => {
   modal1.style.display = 'none';
 });
 
-// Close modal 2 (X)
 closeBtn2.addEventListener('click', () => {
   modal2.style.display = 'none';
 });
 
-// Click outside modals to close
 window.addEventListener('click', e => {
   if (e.target === modal1) modal1.style.display = 'none';
   if (e.target === modal2) modal2.style.display = 'none';
 });
 
-// On "Зберегти" submit
 saveBtn.addEventListener('click', function (e) {
   e.preventDefault();
   const name = input.value.trim();
@@ -34,7 +29,7 @@ saveBtn.addEventListener('click', function (e) {
   if (name) {
     localStorage.username = name;
     if (userNameDisplay) {
-      userNameDisplay.textContent = `Привіт, ${name}!`;
+      userNameDisplay.textContent = name;
     }
     modal1.style.display = 'none';
     modal2.style.display = 'block';
