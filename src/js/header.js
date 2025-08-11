81ac46baedc7017d5abc304ec5a19fca18d162bf
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2025-08-11 21:02:01 +0300

    bugfix

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,31 @@
-import {games} from "../data/game-carts-data"
+const filterList = document.getElementById("header-submenu-filter-list");
+const sections = document.querySelectorAll("main section");
+const filterBtn = document.getElementById("nav-link");
 
-const numericalBtn = document.getElementById("numerical")
+
+filterList.addEventListener("click", onFilterItemClick);
+filterBtn.addEventListener("click", onAllButtonClick);
+
+
+function onFilterItemClick(event){
+    
+    const filterTarget = event.target.id;
+
+    if(filterTarget === "numerical" || filterTarget === "game" || filterTarget === "acquaintance"){
+        sections.forEach(section => {
+            if(section.dataset.category === filterTarget){
+                section.style.display = "block";
+            } else {
+                section.style.display = "none";
+            }
+        }) 
+    }
+}
+
+
+function onAllButtonClick(event){
+    event.preventDefault();
+    sections.forEach(section => {
+        section.style.display = "block";
+    });
+}
