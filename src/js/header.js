206aba622dbcc1d8b7ddb216625d17ef135ebd38
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-08-12 18:51:18 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,9 +1,3 @@
 import {games} from "../data/game-carts-data"
 
 const numericalBtn = document.getElementById("numerical")
-const gameBtn = document.getElementById("game")
-const acquaintanceBtn = document.getElementById("acquaintance")
-
-numericalBtn, gameBtn, acquaintanceBtn.addEventListener ("click", (e) => {
-  
-})
